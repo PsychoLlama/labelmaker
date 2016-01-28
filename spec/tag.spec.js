eb7c1ce5ef763b06beb62623fb4d8f3ae4a9bc49
Jesse Gibson <Jesse_Gibson@me.com>
2016-01-28 14:20:34 -0700

    Added tests, fix universal tag cataloging, clean up after tests, rename for npm, update dependency, add test warning and log squelching, timeout interval, update build.
    Finally got the tests up and running with jasmine. The problem was that I was using an older version of gun without implicit key/path blazing, and my tests were timing out. Updating to the gun@v0.3 build fixed that problem.
    
    After all the tests run, delete the test data `json` file.
    
    Changed the name to something more tolerable on npm, although I still am not happy with the name. Oh well :(
    
    Added a warning on the top of the test file warning about test compatibility with gun@v0.2.
    
    The timeout interval has been updated to 1 second.
    
    The peer connection warnings are now squelched from logging.

--- spec/tag.spec.js
+++ spec/tag.spec.js
@@ -1,24 +1,102 @@
-/*global describe, it, expect, jasmine*/
-/*jslint node: true*/
+/*global describe, it, expect, jasmine, beforeEach, afterAll*/
+/*jslint node: true, nomen: true*/
 'use strict';
 
+/*
+  Warning:
+  These tests are written for
+  gun@v0.3, and may not work
+  for older versions.
+*/
+
+// Squelch gun peer connection warnings
+console.log = (function () {
+  var log = console.log;
+  return function (name) {
+
+    if (name && name.match(/Warning!/i)) {
+      return;
+    }
+
+    log.apply(console, arguments);
+  };
+}());
+
+
+// set the default timeout
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
+
 var tagger = require('../index');
-var Gun = require('gun/gun');
+var scope = require('../lib/scope');
+var Gun = require('gun');
+tagger(Gun);
 
-describe('Tagger', function () {
+describe('The tagger function', function () {
+  var gun, tag;
 
-  it('should be a function', function () {
-    expect(tagger).toEqual(jasmine.any(Function));
+  beforeEach(function () {
+    // generate a new tag each test
+    tag = Gun.text.random(10);
 
-    tagger(Gun);
+    // make a new gun instance
+    gun = new Gun({
+      file: 'tag.spec-data.json'
+    });
   });
 
   it('should export a "tag" method', function () {
     expect(Gun.chain.tag).toEqual(jasmine.any(Function));
   });
 
-  it('should export a "find" method', function () {
-    expect(Gun.chain.find).toEqual(jasmine.any(Function));
+  it('should export a "tagged" method', function () {
+    expect(Gun.chain.tagged).toEqual(jasmine.any(Function));
   });
 
+
+  describe('tag method', function () {
+
+    it('should index data', function (done) {
+      gun.put({
+        name: 'success'
+      }).tag(tag).get(scope + tag).map().val(done);
+    });
+
+    it('should not pseudo-merge nodes', function (done) {
+      gun.put({
+        data: true
+      }).tag(tag).get(scope + tag).map().val(function (val) {
+        expect(val.name).toBe(undefined);
+        expect(val.data).toBe(true);
+        done();
+      });
+    });
+
+    it('should be able to take multiple tags', function (done) {
+      gun.put({
+        test: true
+      }).tag(tag, 'custom');
+      gun.tagged('custom').val(done);
+    });
+
+  });
+
+  describe('tagged method', function () {
+
+    it('should be able to find tags', function (done) {
+      gun.put({
+        data: true
+      }).tag(tag);
+      gun.tagged(tag).map().val(done);
+    });
+
+    it('should be able to find every tag', function (done) {
+      gun.tagged().map().val(done);
+    });
+
+  });
+
+});
+
+afterAll(function () {
+  require('fs').unlink(__dirname + '/../tag.spec-data.json');
 });
